6ca514f6c6bb5369c13bd7075b09c6857006a445
unknown <wiktor.dabrowa98@gmail.com>
2022-08-02 01:15:17 +0200

    added 'random entry' function

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,6 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry_name>", views.entry, name = "entry"),
-    path("create", views.create, name="create")
+    path("create", views.create, name="create"),
+    path("random", views.random_page, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,10 @@
 
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from . import util
 from django import forms
-    
+import random
+
 class NewPageForm(forms.Form):
     title = forms.CharField(label="title")
     content = forms.CharField(widget=forms.Textarea, label="content")
@@ -22,16 +23,40 @@ def create(request):
     if request.method == "POST":
         form = NewPageForm(request.POST)
         if form.is_valid():
-            current = util.list_entries()
             title = form.cleaned_data["title"]
             content = form.cleaned_data["content"]
+
+            #Checking if a wiki entry with the 
+            #same name already exists
+            current = util.list_entries()
             for entry in current:
-                
                 if entry.lower() == title.lower():
                     return HttpResponse("Wiki entry with that name already exist.")
                 else:
                     continue
-            
         util.save_entry(title,content)
+        return redirect('/')
     return render(request, "encyclopedia/create.html", {"form": NewPageForm()})
 
+def random_page(request):
+    list = util.list_entries()
+    length = len(list)
+    random_number = random.randrange(1, length+1)
+    entries=[]
+    i=1
+    for entry in list:
+        pair=[]
+        pair.append(entry)
+        pair.append(i)
+        i += 1
+        entries.append(pair)
+    for pair in entries:
+        if pair[1] == random_number:
+            correct_entry = pair[0]
+            title = pair[0]
+        else:
+            continue        
+    entry = util.get_entry(correct_entry)
+    return render(request, "encyclopedia/entry.html", {"entry" : entry, "title":title})
+    #return render(request, "encyclopedia/entry.html", {"entry": context, "title":"title"})
+    
